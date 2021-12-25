d4b0609763d6c550d4a6d1c26a27816b554c5f11
kiemluc01 <kiemluc01@gmail.com>
2021-12-25 11:08:27 +0700

    AVT

--- Views/components/personal/index.php
+++ Views/components/personal/index.php
@@ -13,7 +13,7 @@
 $personal = loadModel('Personal');
 ?>
 <div class="personal">
-    <a href="index.php?cat=personal&sub_cat=person_infor" class="personal">
+    <a href="<?php echo 'index.php?cat=personal&sub_cat=person_infor&user=' . $_REQUEST['user']; ?>" class="personal">
         <img src="<?php $personal->loadIMG($_REQUEST['user']); ?>" id="personal" alt="" style=" width: 60px; height: 60px; border-radius: 50%/50%;">
     </a>
     <h3><?php $personal->loadName($_REQUEST['user']); ?></h3>
--- Views/components/personal/person_infor.php
+++ Views/components/personal/person_infor.php
@@ -1,14 +1,15 @@
+<?php
+$person = loadModel('Personal');
+
+?>
 <div class="menu_container">
     <div id="menu">
         <?php loadModule('menu'); ?>
     </div>
-    <div id="content">
-        <div id="BGR_IMG">
-
-        </div>
-        <div class="content">
-
-        </div>
+    <div id="person">
+        <img src="<?php $person->loadIMG($_REQUEST['user']); ?>" alt="" id="AVT">
+        <div id="BGR"></div>
+        <div id="infor"></div>
     </div>
 
 </div>
