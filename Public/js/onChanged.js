22cb30d2c2f883a7b28ac7ca3a40a39a8ea31d9e
kiemluc01 <kiemluc01@gmail.com>
2022-01-02 10:01:25 +0700

    done 2

--- Public/js/onChanged.js
+++ Public/js/onChanged.js
@@ -48,21 +48,30 @@ $(document).ready(function() {
             url: "Models/spend_json_data.php",
             dataType: 'json',
             success: function(data) {
-                var str = '<table id="content_table_spend"><tr> <th id="STT">STT</th> <th id="Loai">Loại chi tiêu</th><th id="content">Nội dung</th><th id="ngay">Ngày mua</th><th id="tong">Tổng tiền</th></tr>'
+                var str = '<select name="month_spend" id="month_spend"><option value="Tất cả" selected>Tất cả</option>'
+                for (i = 0; i < data.length; i++) {
+                    var spend = data[i]
+                    str = str + '<option value="' + spend['ngaymua'].substr(0, 7) + '">' + spend['ngaymua'].substr(0, 7) + '</option>'
+                }
+
+                str = str + '</select><table id="content_table_spend"><tr> <th id="STT">STT</th> <th id="Loai">Loại chi tiêu</th><th id="content">Nội dung</th><th id="ngay">Ngày mua</th><th id="tong">Tổng tiền</th></tr>'
 
                 var tien = 0;
+                var stt = 0;
                 for (i = 0; i < data.length; i++) {
                     var spend = data[i]
                     var ngay = spend['ngaymua'].substr(0, 7)
                     if (ngay == text) {
                         if (idND == spend['idND']) {
-                            str = str + "<tr><td>" + (i + 1) + "</td><td>" + spend['Loai'] + "</td><td>" + spend['noidung'] + "</td><td>" + spend['ngaymua'] + "</td><td>" + spend['tongtien'] + " VNĐ</td></tr>"
+                            stt++
+                            str = str + "<tr><td>" + stt + "</td><td>" + spend['Loai'] + "</td><td>" + spend['noidung'] + "</td><td>" + spend['ngaymua'] + "</td><td>" + spend['tongtien'] + " VNĐ</td></tr>"
                             tien += Number(spend['tongtien'])
                         }
                     }
                 }
                 str = str + '<tr><td colspan="3">Tổng tiền</td><td colspan="2">' + tien + ' VNĐ</td></tr></table>'
-                    // $('#content_table_spend').remove()
+
+                // $('#content_table_spend').remove()
                 document.getElementById('content_table_spend').style.display = 'none'
                 document.getElementById('table_spend').innerHTML = str
 
